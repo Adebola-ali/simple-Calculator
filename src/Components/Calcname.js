352d49325f9f29784149067cf469626374e6874b
Adebola-ali <adebolabiz@gmail.com>
2022-08-15 10:34:55 +0100

    commit

---
+++ src/Components/Calcname.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Calcname.css";
+
+const Calcname = () => {
+  return (
+    <div className='calc-info'> 
+       <h4> Adebola's Calculator</h4>
+    </div>
+  )
+}
+
+export default Calcname
